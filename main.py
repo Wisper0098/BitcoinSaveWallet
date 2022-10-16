a260815cb58862416bf553afb42e0c33eeee6933
unknown <hansioulua416@gmail.com>
2022-10-16 22:11:39 +0300

    ghostik-hvostik

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_folder='static')
+
+
+
+@app.route('/')
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
